68b77c09f5244be8a4cf2329ad22e7d1e7735f08
Alvin Junianto Lan <alvin.junianto@gmail.com>
2016-11-01 21:59:12 +0700

    add save model

--- WekaTest.java
+++ WekaTest.java
@@ -57,6 +57,20 @@ public class WekaTest {
  		System.out.println(eval.toClassDetailsString());
  		System.out.println(eval.toMatrixString());
 	}
+	
+	public static void saveModel(String modelname) throws Exception {
+		String outname = "";
+		outname = outname.concat(modelname);
+		outname = outname.concat(".model");
+		weka.core.SerializationHelper.write(outname, train);
+	}
+	
+	/*
+	public static void loadModel(String modelfile) throws Exception {
+		NaiveBayes classifier = (NaiveBayes) weka.core.SerializationHelper.read(modelfile);
+	}
+	*/
+	
 
 	public static void main(String args[]) throws Exception {
 		System.out.print("Please input path to dataset : ");
@@ -67,9 +81,17 @@ public class WekaTest {
 		// load data
 	    load(filename);
 
-	    //setup discretize filter
-	    discretize();
-
+		
+	    //setup filter
+		System.out.println("Filter 0. No 1. Discretisize 2. NumericToNominal");
+		System.out.print("Use filter : ");
+		String fvar = sc.next();
+		if ("1".equals(fvar)) {
+			discretize();
+		} 
+		else if ("2".equals(fvar)) {
+			//numericToNominal();
+		}
 
 	    // train NaiveBayes
 	    //NaiveBayes nb = new NaiveBayes();
@@ -77,11 +99,29 @@ public class WekaTest {
 	    naiveBayes();
 	    //System.out.println(classifier);
 	    
+		//use schema
+		System.out.println("Schema 1. 10-fold Cross Validate 2. Full Training");
+		System.out.print("Use schema : ");
+		String svar = sc.next();
+		if ("1".equals(svar)) {
+			crossValidate();
+		} 
+		else if ("2".equals(svar)) {
+			fulltraining();
+		}
 
-	    //crossValidate();
-	    fulltraining();
 	    //eval.evaluateModel(nb, structure);
 	    printEvalResult();
+		
+		//save model
+		System.out.print("Want to save model?(Y/N) : ");
+		String savevar = sc.next();
+		if ("Y".equals(savevar)) {
+			System.out.print("model name : ");
+			String modelname = sc.next();
+			saveModel(modelname);
+			System.out.println("model saved");
+		}
  		
 	}
 }
